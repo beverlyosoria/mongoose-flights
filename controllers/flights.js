38986a4dafcd0358f6aee88c71655c24cd0b101f
beverlyosoria <bevosoria@gmail.com>
2019-08-17 17:23:53 -0500

    adds create and new

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,11 +1,25 @@
 var Flight = require("../models/flight");
 
 module.exports = {
-  index
+  index,
+  create,
+  new: newFlight
 };
 
 function index(req, res) {
   Flight.find({}, function(err, flights) {
     res.render("flights/index", { title: "All Flights", flights });
+  });
+}
+function newFlight(req, res) {
+  res.render("flights/new", { title: "Add Flight" });
+}
+
+function create(req, res) {
+  var flight = new Flight(req.body);
+  flight.save(function(err) {
+    if (err) return res.redirect("/flights/new");
+    console.log(flight);
+    res.redirect("/flights");
   });
 }
--- routes/flights.js
+++ routes/flights.js
@@ -5,5 +5,7 @@ var flightsCtrl = require("../controllers/flights");
 /* GET users listing. */
 
 router.get("/", flightsCtrl.index);
+router.get("/new", flightsCtrl.new);
+router.post("/", flightsCtrl.create);
 
 module.exports = router;
