dafadd460c6f8652e6c0ac5afc8d58fe1468a1cc
beverlyosoria <bevosoria@gmail.com>
2019-08-17 19:11:20 -0500

    adds show

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,8 +3,15 @@ var Flight = require("../models/flight");
 module.exports = {
   index,
   create,
-  new: newFlight
+  new: newFlight,
+  show
 };
+
+function show(req, res) {
+  Flight.findById(req.params.id, function(err, flight) {
+    res.render("flights/show", { title: "Flight Detail", flight });
+  });
+}
 
 function index(req, res) {
   Flight.find({}, function(err, flights) {
